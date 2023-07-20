88ad7f2e534fc98c00018a152602c6c38b573a6f
vsedtech <hello.vsedtech@gmail.com>
2023-07-20 17:46:57 +0530

    added a few lines

--- src/com/company/SecondClass.java
+++ src/com/company/SecondClass.java
@@ -4,5 +4,8 @@ public class SecondClass {
     public static void main(String... args){
         System.out.println("HI");
         System.out.println("SAN");
+        int a = 1;
+        int b = 10;
+        System.out.println(a+b+"is the result of adding a and b");
     }
 }
