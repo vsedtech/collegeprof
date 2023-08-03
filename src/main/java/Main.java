8d2061528c98d40c32d367a708d40e4b83d22fd3
vsedtech <hello.vsedtech@gmail.com>
2023-08-03 11:32:56 +0530

    Made some changes to the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,23 @@
+import com.company.SecondClass;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
       System.out.println("what happens");
+      one();
+      two();
+      SecondClass sc = new SecondClass();
+      SecondClass.main();
+
+
     }
     public static void one(){
+        System.out.println("One");
         two();
     }
     public static void two(){
+        System.out.println("Two");
         one();
     }
 }
